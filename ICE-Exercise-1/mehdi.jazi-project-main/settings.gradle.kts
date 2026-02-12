c9dcab89d6924de0b25d61e3cdee756ae0d7dd5b
Mehdi Jazi <mahdi.jazi@gmail.com>
2026-02-12 12:59:31 -0500

    Add ICE Exercise 1

---
+++ ICE-Exercise-1/mehdi.jazi-project-main/settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name = "microservice-parent"
+
+include("product-service")
+
---
+++ ICE-Exercise-1/mehdi.jazi-project-main/src/test/java/ca/gbc/comp3095/microservicesparent/MicroservicesParentApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.microservicesparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
